4adf4400650e923d1417d883ba0c63ffd5edeac3
k-mitkov <krasi0m0@gmail.com>
2021-02-22 21:24:54 +0200

    Initial commit

---
+++ src/test/java/GamesDemo/GamesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package GamesDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
