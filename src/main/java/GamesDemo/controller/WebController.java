8e0aa2c03c8e82fd083a6696524f9b473add990f
Vladislav <v.bachvarov@web.de>
2021-03-01 17:30:29 +0100

    Middle Commit

--- src/main/java/GamesDemo/controller/WebController.java
+++ src/main/java/GamesDemo/controller/WebController.java
@@ -1,4 +1,4 @@
-package WebController;
+package GamesDemo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,10 +8,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class WebController {
 
-    @GetMapping("/greeting")
+    @GetMapping("/")
+    public String index()
+    {
+        return "index";
+    }
+
+    @GetMapping("/hello")
     public String greeting(@RequestParam(name="name", required = false,
     defaultValue = "World") String name, Model model )
     {
+        System.out.println("Greeting");
         model.addAttribute("name", name);
         return "greeting";
     }
