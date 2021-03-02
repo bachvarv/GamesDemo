3bc3137d97492001660ded832d6995e0257d6e9e
Vladislav <v.bachvarov@web.de>
2021-03-02 20:42:52 +0100

    Finally Made it Work

--- src/main/java/GamesDemo/GamesDemoApplication.java
+++ src/main/java/GamesDemo/GamesDemoApplication.java
@@ -2,14 +2,22 @@ package GamesDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GamesDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GamesDemoApplication.class, args);
     }
 
+    @GetMapping("/")
+    public String index()
+    {
+        return "index";
+    }
 }
 
 
--- src/main/java/GamesDemo/controller/WebController.java
+++ src/main/java/GamesDemo/controller/WebController.java
@@ -4,21 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class WebController {
 
-    @GetMapping("/")
-    public String index()
-    {
-        return "index";
-    }
 
-    @GetMapping("/hello")
-    public String greeting(@RequestParam(name="name", required = false,
+    @GetMapping(value = "/greeting")
+    public String greetingMethod(@RequestParam(name="name", required = false,
     defaultValue = "World") String name, Model model )
     {
-        System.out.println("Greeting");
         model.addAttribute("name", name);
         return "greeting";
     }
